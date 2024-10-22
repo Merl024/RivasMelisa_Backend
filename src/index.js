9371f7464077178cea3286548be211f967e23aa8
Melisa Rivas <rivasmelisa2024@gmail.com>
2024-10-22 09:59:47 -0600

    empezando con los primeros endpoints

---
+++ src/index.js
@@ -0,0 +1,124 @@
+/**Descripción General
+ Desarrollar un servidor que contenga los endpoints y servicios necesarios para gestionar 
+ los productos y carritos de compra para tu API.
+ 
+ ######## Requisitos de la Primera Entrega ##########
+ Desarrollo del Servidor
+ El servidor debe estar basado en Node.js y Express, y debe escuchar en el puerto 8080. 
+ Se deben disponer dos grupos de rutas: /products y /carts. 
+ Estos endpoints estarán implementados con el router de Express, con las siguientes especificaciones:
+ */
+import express from 'express'
+const app = express()
+const PORT = 8080
+
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+/*
+Rutas para Manejo de Productos (/api/products/)
+GET /:
+Debe listar todos los productos de la base de datos.
+*/
+
+const productos = []
+
+app.get('/api/productos', (req, res) => {
+    res.send(productos)
+})
+
+
+//GET /:pid:
+//Debe traer solo el producto con el id proporcionado.
+app.get('/api/productos/:productId', (req, res) => {
+    let productId = parseInt(req.params.productId)
+
+    const product = productos.find(u => u.productId === productId)
+
+    if (!product){
+        res.status(404).send('Product Not Found')
+    }
+    
+    res.send({ status: "Success", msg: "Aqui esta su producto", data: product})
+
+})
+
+/*
+POST /:
+Debe agregar un nuevo producto con los siguientes campos:
+id: Number/String (No se manda desde el body, se autogenera para asegurar que nunca se repitan los ids).
+title: String
+description: String
+code: String
+price: Number
+status: Boolean
+stock: Number
+category: String
+thumbnails: Array de Strings (rutas donde están almacenadas las imágenes del producto).
+*/
+app.post('/api/productos', (req, res) => {
+    let product = req.body
+
+    // Validando primero que el ID no se vaya a repetir
+    const unicoId = () => {
+        const numRandom = Math.floor(Math.random() * 400 + 1)
+        return product.find(p => p.id === numRandom) ? unicoId(): numRandom
+    }
+
+    if (!product.title || !product.description || !product.code || !product.price || product.status || product.stock || product.stock || product.category || product.thumbnails){
+        return res.status(400).send('Todos los campos son obligatorios')
+    }
+
+    productos.push(product)
+    res.send({status: "success", msg: "Usuario creado", data: product})
+})
+
+
+/*
+PUT /:pid:
+Debe actualizar un producto por los campos enviados desde el body. No se debe actualizar ni eliminar el idal momento de hacer la actualización.
+
+
+DELETE /:pid:
+Debe eliminar el producto con el pid indicado.
+
+
+Rutas para Manejo de Carritos (/api/carts/)
+POST /:
+Debe crear un nuevo carrito con la siguiente estructura:
+id: Number/String (Autogenerado para asegurar que nunca se dupliquen los ids).
+
+products: Array que contendrá objetos que representen cada producto.
+
+
+
+GET /:cid:
+Debe listar los productos que pertenecen al carrito con el cid proporcionado.
+
+
+POST /:cid/product/:pid:
+Debe agregar el producto al arreglo products del carrito seleccionado, utilizando el siguiente formato:
+product: Solo debe contener el ID del producto.
+
+quantity: Debe contener el número de ejemplares de dicho producto (se agregará de uno en uno).
+
+
+Si un producto ya existente intenta agregarse, se debe incrementar el campo quantity de dicho producto.
+
+
+Persistencia de la Información
+La persistencia se implementará utilizando el sistema de archivos, donde los archivos products.json y carts.json respaldarán la información.
+
+Se debe utilizar el ProductManager desarrollado en el desafío anterior y 
+crear un CartManager para gestionar el almacenamiento de estos archivos JSON.
+
+
+*/
+
+
+app.listen(PORT, () => {
+    console.log(`Se esta escuchando en el puerto ${PORT}`);
+    
+})
